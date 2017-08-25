e810e4b8ffd1af7a4da7f594e5b86271a181efcd
pouya-moh <other.pouya@gmail.com>
2017-08-25 16:01:59 +0200

    basic version, needs testing
    seems to be ready, however, no test is done with the real robot yet.
    there are several instances of setToKRLInt and getFrmKRLInt which
    is not documented but I'm doing it anyway since I'm trying to follow
    the standard set by others here.
    	* check the added torques and redundancy resolution pointers
    	1. Check the stophook
    	2. Make sense of configureHook
    	3. Proper initialization (e.g., getting from robot)

--- include/RttFriCartImpedCtrl.hpp
+++ include/RttFriCartImpedCtrl.hpp
@@ -14,6 +14,7 @@
 #include <rst-rt/dynamics/JointImpedance.hpp>
 #include <rst-rt/dynamics/JointTorques.hpp>
 #include <rst-rt/kinematics/JointAngles.hpp>
+#include <kdl/frames.hpp>
 #include "friremote_rt.h"
 
 #define DOF_SIZE 7
@@ -33,11 +34,15 @@ private:
     friRemote _fri;
     std::string _host_ip, _server_ip;
     int _port;
+    bool _recieved, _command;
+    double time, old_time;
 
     // input ports
-    RTT::InputPort<rstrt::geometry::Pose> cartesian_pose_in_port;
+    RTT::InputPort<KDL::Frame> cartesian_pose_in_port;
     RTT::FlowStatus cartesian_pose_in_flow;
-    rstrt::geometry::Pose cartesian_pose_in_data;
+    KDL::Frame cartesian_pose_in_data;
+//    std::vector<float> robot_data;
+    float cart_data_robot[FRI_CART_FRM_DIM];
 
     RTT::InputPort<rstrt::dynamics::JointImpedance> stiff_damp_in_port;
     RTT::FlowStatus stiff_damp_in_flow;
@@ -51,15 +56,29 @@ private:
     RTT::FlowStatus redundancy_resolution_in_flow;
     rstrt::kinematics::JointAngles redundancy_resolution_in_data;
 
+    float * torques;
+    float * joints;
+
     // output ports
-    RTT::OutputPort<rstrt::geometry::Pose> cartesian_pose_out_port;
-    rstrt::geometry::Pose cartesian_pose_out_data;
+    RTT::OutputPort<KDL::Frame> cartesian_pose_out_port;
+    KDL::Frame cartesian_pose_out_data;
+
+    RTT::OutputPort<rstrt::kinematics::JointAngles> current_joint_values_out_port;
+    rstrt::kinematics::JointAngles current_joint_values_out_data;
+
+    RTT::OutputPort<rstrt::dynamics::JointTorques> current_joint_torques_out_port;
+    rstrt::dynamics::JointTorques current_joint_torques_out_data;
+
+
     // maybe add stiffness and damping
     // deffinitly add the currenc conf and currecnt torques
 
 
     void initPorts();
     void sense();
+    bool getCommand();
+    void move();
+    void form_output_data();
     rstrt::geometry::Translation _ee_position;
     rstrt::geometry::Rotation _ee_orientation;
 protected:
