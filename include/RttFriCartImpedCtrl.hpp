9e21ece0522a5a2bb59c2413097ee28dba551b07
pouya-moh <other.pouya@gmail.com>
2017-08-25 12:04:50 +0200

    w.i.p before adding fri stuff
    we should add some feedback ports for current joint position,
    velocity and torques. I am trusting the current code at corlab repo
    but might need revising.

--- include/RttFriCartImpedCtrl.hpp
+++ include/RttFriCartImpedCtrl.hpp
@@ -10,6 +10,13 @@
 #define RTTFRICARTIMPEDCTRL_HPP
 
 #include <rtt/RTT.hpp>
+#include <rst-rt/geometry/Pose.hpp>
+#include <rst-rt/dynamics/JointImpedance.hpp>
+#include <rst-rt/dynamics/JointTorques.hpp>
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include "friremote_rt.h"
+
+#define DOF_SIZE 7
 
 class RttFriCartImpedCtrl : public RTT::TaskContext{
 public:
@@ -19,8 +26,42 @@ public:
     void updateHook();
     void stopHook();
     void cleanupHook();
-private:
 
+
+
+private:
+    friRemote _fri;
+    std::string _host_ip, _server_ip;
+    int _port;
+
+    // input ports
+    RTT::InputPort<rstrt::geometry::Pose> cartesian_pose_in_port;
+    RTT::FlowStatus cartesian_pose_in_flow;
+    rstrt::geometry::Pose cartesian_pose_in_data;
+
+    RTT::InputPort<rstrt::dynamics::JointImpedance> stiff_damp_in_port;
+    RTT::FlowStatus stiff_damp_in_flow;
+    rstrt::dynamics::JointImpedance stiff_damp_in_data;
+
+    RTT::InputPort<rstrt::dynamics::JointTorques> added_torques_in_port;
+    RTT::FlowStatus added_torques_in_flow;
+    rstrt::dynamics::JointTorques added_torques_in_data;
+
+    RTT::InputPort<rstrt::kinematics::JointAngles> redundancy_resolution_in_port;
+    RTT::FlowStatus redundancy_resolution_in_flow;
+    rstrt::kinematics::JointAngles redundancy_resolution_in_data;
+
+    // output ports
+    RTT::OutputPort<rstrt::geometry::Pose> cartesian_pose_out_port;
+    rstrt::geometry::Pose cartesian_pose_out_data;
+    // maybe add stiffness and damping
+    // deffinitly add the currenc conf and currecnt torques
+
+
+    void initPorts();
+    void sense();
+    rstrt::geometry::Translation _ee_position;
+    rstrt::geometry::Rotation _ee_orientation;
 protected:
 
 };
--- src/RttFriCartImpedCtrl.cpp
+++ src/RttFriCartImpedCtrl.cpp
@@ -9,12 +9,58 @@
 #include "RttFriCartImpedCtrl.hpp"
 #include <rtt/Component.hpp>
 
-RttFriCartImpedCtrl::RttFriCartImpedCtrl(std::string const& name) : TaskContext(name){
+RttFriCartImpedCtrl::RttFriCartImpedCtrl(std::string const& name) : TaskContext(name){    
+    _server_ip = "192.168.0.200";
+    addProperty("server_ip", _server_ip).doc("IP of this machine");
 
+    _host_ip = "192.168.0.21";
+    addProperty("host_ip", _host_ip).doc("IP of robot");
+
+    _port = 49938;
+    addProperty("port", _port).doc("server port");
+
+    initPorts();
 }
 
-bool RttFriCartImpedCtrl::configureHook(){
+void RttFriCartImpedCtrl::initPorts(){
+    cartesian_pose_in_flow = RTT::NoData;
+    cartesian_pose_in_port.setName("des_cart_pose");
+    cartesian_pose_in_data = rstrt::geometry::Pose(0,0,0,1,0,0,0);
+    ports()->addPort(cartesian_pose_in_port);
 
+    stiff_damp_in_flow = RTT::NoData;
+    stiff_damp_in_port.setName("des_stiff_damp");
+    stiff_damp_in_data = rstrt::dynamics::JointImpedance(DOF_SIZE);
+    ports()->addPort(stiff_damp_in_port);
+
+    added_torques_in_flow = RTT::NoData;
+    added_torques_in_port.setName("des_added_torques");
+    added_torques_in_data = rstrt::dynamics::JointTorques(DOF_SIZE);
+    ports()->addPort(added_torques_in_port);
+
+    redundancy_resolution_in_flow = RTT::NoData;
+    redundancy_resolution_in_port.setName("des_null_space");
+    redundancy_resolution_in_data = rstrt::kinematics::JointAngles(DOF_SIZE);
+    ports()->addPort(redundancy_resolution_in_port);
+}
+
+bool RttFriCartImpedCtrl::configureHook(){    
+    // TODO check the fourth argument here
+    _fri = friRemote(_port, _host_ip.c_str(), _server_ip.c_str(), this->getActivity()->thread()->getTask());
+    // TODO make a sense of this:
+    char str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(_fri.remote.krcAddr.sin_addr), str, INET_ADDRSTRLEN);
+    RTT::log(RTT::Info) << "Opening FRI Version "
+                        << FRI_MAJOR_VERSION << "."
+                        << FRI_SUB_VERSION << "."
+                        << FRI_DATAGRAM_ID_CMD << "."
+                        << FRI_DATAGRAM_ID_MSR
+                        << " Interface for LWR ROS server"
+                        << RTT::endlog();
+    RTT::log(RTT::Info) << "Checking if the robot is Stopped..." << RTT::endlog();
+    if( this->_fri_inst->getState() == FRI_STATE_OFF ){
+         RTT::log(RTT::Info) << "Please, start the KRL script now." << RTT::endlog();
+    }
 	return true;
 }
 
@@ -24,7 +70,7 @@ bool RttFriCartImpedCtrl::startHook(){
 }
 
 void RttFriCartImpedCtrl::updateHook(){
-
+    sense();
 }
 
 void RttFriCartImpedCtrl::stopHook() {
@@ -33,6 +79,10 @@ void RttFriCartImpedCtrl::stopHook() {
 
 void RttFriCartImpedCtrl::cleanupHook() {
 
+}
+
+void RttFriCartImpedCtrl::sense(){
+    
 }
 
 /*
