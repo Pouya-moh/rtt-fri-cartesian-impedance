8ab9629828f07294d9bb4a1969fd9b8663d20c96
pouya-moh <other.pouya@gmail.com>
2017-08-25 10:25:08 +0200

    add files, sould turn them to components

---
+++ include/RttFriCartImpedCtrl.hpp
@@ -0,0 +1,23 @@
+/*
+ * Author:  Pouya Mohammadi
+ * Date:    August 25, 2017
+ * License: 
+ * Description: 
+ * 
+ */
+
+#ifndef RTTFRICARTIMPEDCTRL_HPP
+#define RTTFRICARTIMPEDCTRL_HPP
+
+class RttFriCartImpedCtrl {
+public:
+	RttFriCartImpedCtrl();
+	RttFriCartImpedCtrl(const RttFriCartImpedCtrl& orig);
+	virtual ~RttFriCartImpedCtrl();
+
+private:
+
+protected:
+
+};
+#endif // RTTFRICARTIMPEDCTRL_HPP
---
+++ src/RttFriCartImpedCtrl.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author:  Pouya Mohammadi
+ * Date:    August 25, 2017
+ * License: 
+ * Description: 
+ * 
+ */
+
+#include "RttFriCartImpedCtrl.hpp"
+
+RttFriCartImpedCtrl::RttFriCartImpedCtrl() {
+	
+}
+
+RttFriCartImpedCtrl::RttFriCartImpedCtrl(const RttFriCartImpedCtrl& orig) {
+	
+}
+
+RttFriCartImpedCtrl::~RttFriCartImpedCtrl() {
+	
+}
